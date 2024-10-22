dda7cf99ce7d18cf26d431aa282d6c3874cd280a
gdhiman1177 <garvdhiman23@gmail.com>
2024-10-22 20:22:58 +0530

    abc

---
+++ index.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st no:"))
+b=int(input("Enter second no"))
+c=a/b
+print("Sum of two no is",c)
+print("multiplication"/n)
+d=int(input("Enter 1st no:"))
+e=int(input("Enter second no"))
+f=d*e
+print("multiplication of two no is",f)
